f2520731b5334eb98cf80a9849fc8cf14bc657b3
akashktesla <akashktesla@gmail.com>
2025-04-06 16:59:08 +0530

    :)

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Use existing Firefox profile
 options = Options()
 options.add_argument("--headless")
 options.add_argument("-profile")
@@ -14,10 +13,8 @@ options.add_argument("/home/akash/.mozilla/firefox/qymcnn0s.default")  # replace
 driver = webdriver.Firefox(service=Service(), options=options)
 driver.get("https://web.whatsapp.com")
 
-# Let the page load a bit
 time.sleep(5)
 
-# Search for a group
 group_name = "A120 4 lyf ❤️"  # replace this with actual group name
 search = driver.find_element(By.XPATH, '//div[@contenteditable="true"]')
 search.click()
@@ -29,6 +26,8 @@ group = driver.find_element(By.XPATH, f'//span[@title="{group_name}"]')
 group.click()
 
 everyone = ["@ani","@siva", "@ct", "@authi","@sneha","@dhana", "@dhiv", "@dins", "@gokul", "@harini", "@jas","@jel", "@karthi", "@po", "@soap","@vats","@vis", "@sriva"]
+girls = ["@ani", "@authi","@sneha", "@dhiv", "@harini" "@sriva"]
+boys = ["@siva", "@ct", "@dhana",  "@dins", "@gokul",  "@jas","@jel", "@karthi", "@po", "@soap","@vats","@vis"]
 flag = True
 while flag:
     messages = driver.find_elements(By.XPATH, '//div[contains(@class, "message-in") or contains(@class, "message-out")]')
@@ -43,6 +42,26 @@ while flag:
             msg_box.send_keys(Keys.TAB)      
             msg_box.send_keys(",")
 
+        msg_box.send_keys(Keys.ENTER)      
+
+    if "@boys" in text.lower():
+        msg_box = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="10"]')
+        msg_box.click()
+        for i in boys:
+            for j in i:
+                msg_box.send_keys(j)
+            msg_box.send_keys(Keys.TAB)      
+            msg_box.send_keys(",")
+        msg_box.send_keys(Keys.ENTER)      
+
+    if "@girls" in text.lower():
+        msg_box = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="10"]')
+        msg_box.click()
+        for i in girls:
+            for j in i:
+                msg_box.send_keys(j)
+            msg_box.send_keys(Keys.TAB)      
+            msg_box.send_keys(",")
         msg_box.send_keys(Keys.ENTER)      
 
     print("Command:",text)
